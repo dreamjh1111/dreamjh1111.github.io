b8cbdf354d4e5d6c144e22dc206600040841b9b7
jonghyuck <dreamjh1111@gmail.com>
2026-03-09 18:10:20 +0900

    update

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -2,6 +2,8 @@ import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
+// @ts-ignore
+import script from "./scripts/pageTitle.inline"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
@@ -18,7 +20,26 @@ PageTitle.css = `
   font-size: 1.75rem;
   margin: 0;
   font-family: var(--titleFont);
+  min-width: 0;
+  line-height: 1.1;
+}
+
+.page-title a {
+  display: block;
+  min-width: 0;
+}
+
+@media all and (max-width: 800px) {
+  .page-title {
+    font-size: var(--page-title-mobile-size, 1.75rem);
+  }
+
+  .page-title a {
+    white-space: nowrap;
+  }
 }
 `
+
+PageTitle.afterDOMLoaded = script
 
 export default (() => PageTitle) satisfies QuartzComponentConstructor
